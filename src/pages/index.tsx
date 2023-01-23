1d7c111c268529ce8d011c967b152c36e6fe6aad
nelson frank <nelsonfr78@outlook.com>
2023-01-23 15:24:41 +0300

    feat: Add css reset

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,8 +13,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="w-full min-h-screen bg-gray-300">
-        <h1 className="text-2xl capitalize">Job finder app</h1>
+      <main className="w-full min-h-screen bg-gray-100">
+        <div className="py-10">
+          <h1 className="text-2xl capitalize rounded-md py-4 px-4 mx-4 inline-flex items-center bg-white">
+            Job finder app
+          </h1>
+        </div>
       </main>
     </>
   );
