f1c075147979701d91d108175d58f008226b464f
nelson frank <nelsonfr78@outlook.com>
2023-01-24 10:52:48 +0300

    feat: Add job post card

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import { FaBookmark } from "react-icons/fa";
+import Image from "next/image";
+import { FiBookmark } from "react-icons/fi";
+
+export interface JobPost {
+  company: string;
+  position: string;
+  location: string;
+  type: string;
+  companyLogo: string;
+  excerpt: string;
+  tags?: string[];
+}
+const Card = ({
+  company,
+  position,
+  location,
+  type,
+  tags,
+  companyLogo,
+  excerpt,
+}: JobPost) => {
+  const [bookmarked, bookmarkSet] = useState<boolean>(false);
+
+  return (
+    <div className="my-6 rounded-2xl bg-white py-4 px-4">
+      <div className="mb-4 flex items-center justify-between">
+        <div className="flex items-center">
+          <div className="mr-2 rounded-lg bg-gray-200 p-3">
+            <Image
+              src={companyLogo}
+              alt="telegram"
+              width={40}
+              height={40}
+              className="rounded-full"
+            />
+          </div>
+          <div>
+            <h4 className="mb-1 font-medium capitalize">{company}</h4>
+            <h2 className="mb-1 text-lg font-bold capitalize">{position}</h2>
+            <p className="text-sm font-light capitalize text-zinc-400">
+              {location} - {type}
+            </p>
+          </div>
+        </div>
+        <div className="rounded-md bg-gray-200 p-2 ">
+          {bookmarked ? (
+            <button
+              className="text-2xl outline-none"
+              onClick={() => bookmarkSet(!bookmarked)}
+            >
+              <FiBookmark />
+            </button>
+          ) : (
+            <button
+              className="text-2xl outline-none"
+              onClick={() => bookmarkSet(!bookmarked)}
+            >
+              <FaBookmark />
+            </button>
+          )}
+        </div>
+      </div>
+      <div className="mb-4 flex items-center">
+        {tags?.map((tag, index) => (
+          <button
+            key={index + 1}
+            className="mr-3 whitespace-nowrap rounded-md bg-gray-200 py-1 px-3 text-sm font-medium capitalize outline-none"
+          >
+            {tag}
+          </button>
+        ))}
+      </div>
+      <div>
+        <p className="text-justify text-sm text-zinc-400">{excerpt}...</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,36 @@
-import { useState } from "react";
 import Head from "next/head";
-import Image from "next/image";
-import { FiBookmark, FiFigma } from "react-icons/fi";
+import { FiFigma } from "react-icons/fi";
 import { TbBrandJavascript } from "react-icons/tb";
-import { FaBookmark } from "react-icons/fa";
 
 // components
 import Header from "@/components/header";
 import Banner from "@/components/banner";
+import Card from "@/components/card";
+import { JobPost } from "@/components/card/Card";
 
+const jobPosts: JobPost[] = [
+  {
+    company: "Telegram",
+    companyLogo: "/img/telegram.png",
+    location: "Jakarta Indonesia",
+    position: "Junior UI Designer",
+    type: "onsite",
+    excerpt:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus atque quo, at deserunt, labore recusandae sint quod modi reiciendis ad mollitia quia delectus ab amet.",
+    tags: ["FullTime", "Internship", "Entry Level"],
+  },
+  {
+    company: "Behance",
+    companyLogo: "/img/behance.png",
+    location: "Surabaya Indonesia",
+    position: "Senior UI Designer",
+    type: "onsite",
+    excerpt:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus atque quo, at deserunt, labore recusandae sint quod modi reiciendis ad mollitia quia delectus ab amet.",
+    tags: ["Contract", "Internship", "Senior Level"],
+  },
+];
 export default function Home() {
-  const [bookmarked, bookmarkSet] = useState<boolean>(false);
-
-  console.log(bookmarked);
   return (
     <>
       <Head>
@@ -21,7 +39,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="min-h-screen w-full bg-gray-100">
+      <main className="min-h-screen w-full bg-gray-100 pb-16">
         <div className="mx-6">
           <Header />
           <Banner />
@@ -44,64 +62,9 @@ export default function Home() {
             </div>
 
             <div className="my-8">
-              <div className="rounded-2xl bg-white py-4 px-4 ">
-                <div className="mb-4 flex items-center justify-between">
-                  <div className="flex items-center">
-                    <div className="mr-2 rounded-lg bg-gray-200 p-3">
-                      <Image
-                        src="/img/telegram.png"
-                        alt="telegram"
-                        width={40}
-                        height={40}
-                        className="rounded-full"
-                      />
-                    </div>
-                    <div>
-                      <h4 className="mb-1 font-medium capitalize">Telegram</h4>
-                      <h2 className="mb-1 text-lg font-bold capitalize">
-                        Junior UI Designer
-                      </h2>
-                      <p className="text-sm font-light text-zinc-400">
-                        Jakarta, Indonesia - Onsite
-                      </p>
-                    </div>
-                  </div>
-                  <div className="rounded-md bg-gray-200 p-2 text-2xl">
-                    {bookmarked ? (
-                      <button
-                        className="outline-none"
-                        onClick={() => bookmarkSet(!bookmarked)}
-                      >
-                        <FiBookmark />
-                      </button>
-                    ) : (
-                      <button
-                        className="outline-none"
-                        onClick={() => bookmarkSet(!bookmarked)}
-                      >
-                        <FaBookmark />
-                      </button>
-                    )}
-                  </div>
-                </div>
-                <div className="mb-4 flex items-center">
-                  <button className="mr-3 rounded-md bg-gray-200 py-1 px-3 text-sm font-medium capitalize outline-none">
-                    Fulltime
-                  </button>
-                  <button className="mr-3 rounded-md bg-gray-200 py-1 px-3 text-sm font-medium capitalize outline-none">
-                    Internship
-                  </button>
-                  <button className="mr-3 rounded-md bg-gray-200 py-1 px-3 text-sm font-medium capitalize outline-none">
-                    Entry Level
-                  </button>
-                </div>
-                <div>
-                  <p className="text-justify text-sm text-zinc-400">
-                    Lorem, ipsum dolor sit amet consectetur pisicing el elit.
-                    tatis tatis maiores voluptates id...
-                  </p>
-                </div>
-              </div>
+              {jobPosts.map((job) => (
+                <Card {...job} key={job.company} />
+              ))}
             </div>
           </div>
         </div>
